8803354643a83a971ba51fd78ed41c15c86d17e5
kitanaya <seb.heinzmann@gmail.com>
2026-09-24 03:53:17 +0200

    batman

---
+++ repl_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "    ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Errorf(
+				"length mismatch: expected %d, got %d",
+				len(c.expected),
+				len(actual),
+			)
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf(
+					"word mismatch: expected %q, got %q",
+					expectedWord,
+					word,
+				)
+			}
+		}
+	}
+}
